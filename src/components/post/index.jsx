112de788956495292af9deb169a4797eee9a502e
giusgo <giuseppeg@uninorte.edu.co>
2025-04-20 18:34:35 -0500

    fix: replies display

--- src/components/post/index.jsx
+++ src/components/post/index.jsx
@@ -70,7 +70,9 @@ function Post({ user, post, handleRefresh }) {
         where("tweetId", "==", post.id)
       );
       const querySnapshot = await getDocs(q);
-      setReplies(querySnapshot.docs.map((doc) => ({ id: doc.id, ...doc.data() })));
+      setReplies(
+        querySnapshot.docs.map((doc) => ({ id: doc.id, ...doc.data() }))
+      );
     };
 
     fetchReplies();
@@ -125,12 +127,12 @@ function Post({ user, post, handleRefresh }) {
           <button className="delete-post" onClick={handleDelete}>
             <svg
               xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 0 24 24"
-            width="16"
-            height="16"
-            fill="currentColor"
-          >
-            <path d="M6 19c0 1.1.9 2 2 2h8c1.1 0 2-.9 2-2V7H6v12zM19 4h-3.5l-1-1h-5l-1 1H5v2h14V4z" />
+              viewBox="0 0 24 24"
+              width="16"
+              height="16"
+              fill="currentColor"
+            >
+              <path d="M6 19c0 1.1.9 2 2 2h8c1.1 0 2-.9 2-2V7H6v12zM19 4h-3.5l-1-1h-5l-1 1H5v2h14V4z" />
             </svg>
           </button>
         )}
@@ -153,19 +155,23 @@ function Post({ user, post, handleRefresh }) {
           </button>
           <span className="like-count">{likeCount}</span>
         </div>
-        {replies.length > 0 && (
-          <div className="replies">
+        <div className="replies">
+          {(replies.length > 0 || user) && (
             <h3>Respuestas ({replies.length})</h3>
-            <div className="replies-container">
+          )}
+          <div className="replies-container">
             {user && (
-              <CreateReply user={user} postId={post.id} handleRefresh={handleRefreshReplies} />
+              <CreateReply
+                user={user}
+                postId={post.id}
+                handleRefresh={handleRefreshReplies}
+              />
             )}
             {replies.map((reply) => (
               <Reply key={reply.id} reply={reply} />
             ))}
-            </div>
           </div>
-        )}
+        </div>
       </div>
     </div>
   );
